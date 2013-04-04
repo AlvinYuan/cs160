e97d3419004640eae25f4b9b52e6e489f415d4d8
alvinyuan <alvin.yuan@berkeley.edu>
2013-04-04 16:51:08 -0700

    Home button text and id set. Map button opens map activity.

--- Gsale/src/edu/berkeley/cs160/gsale/HomeActivity.java
+++ Gsale/src/edu/berkeley/cs160/gsale/HomeActivity.java
@@ -2,7 +2,9 @@ package edu.berkeley.cs160.gsale;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -17,6 +19,18 @@ public class HomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_home, menu);
 		return true;
+	}
+
+	/*
+	 * Button onClick methods
+	 */
+
+	/*
+	 * Method: ViewMapButton
+	 */
+	public void ViewMapButtonOnClick(View view) {
+		Intent intent = new Intent(this, MapActivity.class);
+		startActivity(intent);
 	}
 
 }
