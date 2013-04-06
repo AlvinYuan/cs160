f51130ce3551a809f3a3b87b0af05bd6d9b7f7c2
alvinyuan <alvin.yuan@berkeley.edu>
2013-04-05 17:17:05 -0700

    basic model

---
+++ Gsale/src/edu/berkeley/cs160/gsale/GarageSale.java
@@ -0,0 +1,13 @@
+package edu.berkeley.cs160.gsale;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GarageSale {
+	public Date startDate;
+	public Date stopDate;
+	public String title;
+	public String description;
+	public User planner;
+	public ArrayList<Photo> photos;
+}
