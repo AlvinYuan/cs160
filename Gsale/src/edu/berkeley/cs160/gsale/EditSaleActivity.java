cb82ad112dd05be63cec2345ec8026ad46f39e2a
alvinyuan <alvin.yuan@berkeley.edu>
2013-04-06 13:44:45 -0700

    more work on edit layout

--- Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
+++ Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
@@ -2,13 +2,23 @@ package edu.berkeley.cs160.gsale;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.support.v4.app.NavUtils;
 
-public class EditSaleActivity extends Activity {
+public class EditSaleActivity extends Activity implements OnSeekBarChangeListener {
 	public boolean isNewSale;
 	public int editingSaleId;
+	public int step;
+	public View editBasicInfoView;
+	public View editDescriptionView;
+	public View editPhotosView;
+	public View editReviewPublishView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,16 @@ public class EditSaleActivity extends Activity {
 		if (!isNewSale) {
 			editingSaleId = extras.getInt(GarageSale.SALE_ID_KEY);
 		}
+		RelativeLayout editLayout = (RelativeLayout) findViewById(R.id.EditLayout);
+		LayoutInflater inflater = getLayoutInflater();
+		editBasicInfoView = inflater.inflate(R.layout.edit_basic_info_view, null);
+		editLayout.addView(editBasicInfoView);
+		
+		SeekBar editProgressBar = (SeekBar) findViewById(R.id.EditProgressBar);
+		editProgressBar.setOnSeekBarChangeListener(this);
+		onProgressChanged(editProgressBar, 0, false); // Trigger on activity creation
+		
+		
 	}
 
 	@Override
@@ -45,6 +65,63 @@ public class EditSaleActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void updateEditView() {
+		switch(step) {
+		case 0:
+			loadBasicInfoView();
+			break;
+		case 1:
+			loadDescriptionView();
+			break;
+		case 2:
+			loadPhotosView();
+			break;
+		case 3:
+			loadReviewPublishView();
+			break;
+		}
+		
+	}
+	
+	public void loadBasicInfoView() {
+
+	}
+	
+	public void loadDescriptionView() {
+		
+	}
+	
+	public void loadPhotosView() {
+		
+	}
+	
+	public void loadReviewPublishView() {
+		
+	}
+	
+	/*
+	 * OnSeekBarChangeListener Methods
+	 */
+	
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		// TODO Auto-generated method stub
+		step = progress;
+		updateEditView();
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
