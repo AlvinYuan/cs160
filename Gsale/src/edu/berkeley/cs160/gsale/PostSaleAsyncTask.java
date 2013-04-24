9855d980f51db6f2178966a60ae9b6f816185e48
monicato <mmonicato@gmail.com>
2013-04-24 01:37:53 -0700

    changed min api level from 13 to 13 in manifest. Need min 16 to use a nice font. Changed the font and made followed star yellow

--- Gsale/src/edu/berkeley/cs160/gsale/PostSaleAsyncTask.java
+++ Gsale/src/edu/berkeley/cs160/gsale/PostSaleAsyncTask.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class PostSaleAsyncTask extends AsyncTask<Void, Void, JSONObject> {
 	public Context context;
@@ -59,6 +60,7 @@ public class PostSaleAsyncTask extends AsyncTask<Void, Void, JSONObject> {
 	protected void onPostExecute(JSONObject result) {
 			int id;
 			try {
+				Log.i("postJson", result+"");
 				id = result.getInt("id");
 				sale.id = id;
 				GarageSale.allSales.put(sale.id, sale);
