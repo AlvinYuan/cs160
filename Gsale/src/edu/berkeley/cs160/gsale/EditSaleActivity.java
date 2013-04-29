8d5820ead5e03b9b8295a8df520377eea0ba73d2
monicato <mmonicato@gmail.com>
2013-04-29 15:34:32 -0700

    implemented dialog to choose gallery or camera when adding photo.

--- Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
+++ Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
@@ -9,15 +9,19 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -60,7 +64,7 @@ public class EditSaleActivity extends FragmentActivity implements OnSeekBarChang
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	public static final int MEDIA_TYPE_VIDEO = 2;
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-	
+	private static final int GALLERY_IMAGE_ACTIVITY_REQUEST_CODE = 101;	
 
 	public boolean selectingStart; //false = selectingEnd (for date/time)
 	
@@ -386,10 +390,36 @@ public class EditSaleActivity extends FragmentActivity implements OnSeekBarChang
 	 * Method: AddNewPhotoButtonOnClick
 	 */
 	public void AddNewPhotoButtonOnClick(View view) {
-	    // create Intent to take a picture and return control to the calling application
-	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    // start the image capture Intent
-	    startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	    
+	    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				this);
+ 
+			// set title
+			alertDialogBuilder.setTitle("Add New Photo");
+ 
+			// set dialog message
+			alertDialogBuilder
+				.setMessage("Choose Photo Option")
+				.setCancelable(true)
+				.setPositiveButton("Camera",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// create Intent to take a picture and return control to the calling application
+					    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+					    // start the image capture Intent
+					    startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+					}
+				  })
+				.setNegativeButton("Gallery",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+						           android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+						startActivityForResult(pickPhoto , GALLERY_IMAGE_ACTIVITY_REQUEST_CODE);
+					}
+				});
+				// Create Alert Dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				// Show Dialog
+				alertDialog.show();
 	}
 	
 	/*
@@ -423,6 +453,37 @@ public class EditSaleActivity extends FragmentActivity implements OnSeekBarChang
 	    	    photoAdapter.notifyDataSetChanged();
 
 	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+	    if (requestCode == GALLERY_IMAGE_ACTIVITY_REQUEST_CODE) {
+	    	if (resultCode == RESULT_OK){
+	    		Toast.makeText(this, "Image Selected!", Toast.LENGTH_LONG).show();
+	    		Uri selectedImage = data.getData();
+	            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+	            Cursor cursor = getContentResolver().query(selectedImage,
+	                    filePathColumn, null, null, null);
+	            cursor.moveToFirst();
+
+	            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	            String picturePath = cursor.getString(columnIndex);
+
+	            cursor.close();
+	            Bitmap imageBitmap = BitmapFactory.decodeFile(picturePath);
+	            Photo p = new Photo();
+	            p.bitmap = imageBitmap;
+	            p.description = "";
+	            
+	            editingSale.mainPhoto = p;
+	    	    editingSale.photos.add(p);
+	    	    
+	    	    photoAdded = true;
+	    	    photoAdapter.notifyDataSetChanged();
+	            
+	    	} else if (resultCode == RESULT_CANCELED) {
 	            // User cancelled the image capture
 	        } else {
 	            // Image capture failed, advise user
