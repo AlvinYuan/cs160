4af0f639d53ca5a11c5dccbdb8e1c7fe4e1f9809
monicato <mmonicato@gmail.com>
2013-05-02 15:58:43 -0700

    removed unused imports

--- Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
+++ Gsale/src/edu/berkeley/cs160/gsale/EditSaleActivity.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -17,7 +16,6 @@ import android.graphics.Point;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -56,9 +54,6 @@ public class EditSaleActivity extends FragmentActivity implements OnSeekBarChang
 	public SeekBar editProgressBar;
 	public Button backButton;
 	public Button nextButton;
-	public ProgressDialog progressBar;
-	private int progressBarStatus = 0;
-	private Handler progressBarHandler = new Handler();
 
 	public ListView editPhotosListView;
 	public PhotoAdapter photoAdapter;
