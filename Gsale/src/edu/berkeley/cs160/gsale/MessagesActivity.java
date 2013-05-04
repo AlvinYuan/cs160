a39939ed541fd348f5b13c1515b6fa624745a0c5
alvinyuan <alvin.yuan@berkeley.edu>
2013-05-04 15:28:14 -0700

    fixed crash of publishing without photo. moved clear shared preferences to settings

--- Gsale/src/edu/berkeley/cs160/gsale/MessagesActivity.java
+++ Gsale/src/edu/berkeley/cs160/gsale/MessagesActivity.java
@@ -1,15 +1,9 @@
 package edu.berkeley.cs160.gsale;
 
-import java.util.ArrayList;
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class MessagesActivity extends Activity {
@@ -20,24 +14,6 @@ public class MessagesActivity extends Activity {
 		setContentView(R.layout.activity_messages);
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		GarageSale testSale1 = new GarageSale();
-		testSale1.title = "SALE SALE SALE";
-		testSale1.location = "testSale1 Location";
-		testSale1.description = "testSale1 Description";
-		testSale1.id = 123456;
-		
-		GarageSale testSale2 = new GarageSale();
-		testSale2.title = "Sale this Saturday";
-		testSale2.location = "testSale2 Location";
-		testSale2.description = "testSale2 Description";
-		testSale2.id = 98765;
-		
-		Storage.storeSale(this, testSale1, testSale1.id);
-		Storage.storeSale(this, testSale2, testSale2.id);
-		
-		Storage.storeId(this, testSale1.id, Storage.FOLLOWED_SALES);
-		Storage.storeId(this, testSale2.id, Storage.FOLLOWED_SALES);
 	}
 
 	@Override
@@ -50,7 +26,6 @@ public class MessagesActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		TextView result = (TextView)findViewById(R.id.storage_test);
 		switch (item.getItemId()) {
 		case android.R.id.home:
 			// This ID represents the Home or Up button. In the case of this
@@ -62,44 +37,8 @@ public class MessagesActivity extends Activity {
 			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
-		case R.id.store_button1:
-			Toast.makeText(this, "You have chosen retrieve.", Toast.LENGTH_SHORT).show();
-			GarageSale temp = Storage.getSale(this, 123456);
-			result.append(temp.title + "\n");
-			return true;
-		case R.id.store_button2:
-			Toast.makeText(this, "You have chosen list.", Toast.LENGTH_SHORT).show();
-			result.setText("");
-			ArrayList<GarageSale> tempSales = Storage.getSales(this, Storage.FOLLOWED_SALES);
-			for(GarageSale g : tempSales) {
-				result.append(g.title + "\n");
-			}
-			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void testList(View view) {
-		TextView result = (TextView)findViewById(R.id.storage_test);
-		result.setText("");
-		ArrayList<Integer> test = Storage.getIds(this, Storage.FOLLOWED_SALES);
-		
-		ArrayList<GarageSale> tempSales = Storage.getSales(this, Storage.FOLLOWED_SALES);
-		for(GarageSale g : tempSales) {
-			result.append(g.title + "\n");
-		}
-	}
-	
-	public void testRetrieve(View view) {
-		TextView result = (TextView)findViewById(R.id.storage_test);
-		GarageSale temp = Storage.getSale(this, 123456);
-		result.append(temp.title + "\n");
-	}
-	
-	public void clearData(View view) {
-		TextView result = (TextView)findViewById(R.id.storage_test);
-		User.currentUser.plannedSales.clear();
-		Storage.clearStorage(this);
-		result.append("Cleared SharedPreferences" + "\n");
-	}
+
 }
